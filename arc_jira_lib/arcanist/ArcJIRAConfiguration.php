908ee556e03d24786cb2926ed93d94bafff15f45
Marek Sapota <mareksapota@fb.com>
2011-10-31 12:12:19 -0700

    Extend SVN workflow
    Summary: Extend SVN `arc diff` workflow.
    
    Test Plan:
    Run `arc diff` or `arc diff --jira ID` in SVN repo.  It should allow ask you if
    you want to connect created diff to an existing revision and if you provided
    `--jira` switch it should allow you to create a new revision and fill in fields
    by pulling info from JIRA.
    
    Reviewers: jsichi
    
    Reviewed By: jsichi
    
    CC: jsichi
    
    Differential Revision: 123

--- arc_jira_lib/arcanist/ArcJIRAConfiguration.php
+++ arc_jira_lib/arcanist/ArcJIRAConfiguration.php
@@ -17,21 +17,38 @@
  */
 
 class ArcJIRAConfiguration extends ArcanistConfiguration {
+  private $workflow;
+  private $repositoryApi;
+  private $conduit;
+  private $jiraId;
+  private $jiraApiUrl;
+  private $jiraInfo;
+  private $comment;
+
   public function willRunWorkflow($command, ArcanistBaseWorkflow $workflow) {
     if ($workflow instanceof ArcanistDiffWorkflow) {
-      $repository_api = $workflow->getRepositoryAPI();
-      // Magic happens only for Git.
-      if (!($repository_api instanceof ArcanistGitAPI)) {
+      $this->workflow = $workflow;
+      $this->repositoryApi = $workflow->getRepositoryAPI();
+      $this->conduit = $workflow->getConduit();
+      $this->getJiraId();
+      // If JIRA parameter is not set, abort custom code execution.
+      if (!$this->jiraId) {
+        return;
+      }
+      $this->getJiraApiUrl();
+      $this->getJiraInfo();
+
+      // Magic in the pre hook happens only for Git.
+      if (!($this->repositoryApi instanceof ArcanistGitAPI)) {
         return;
       }
 
-      $conduit = $workflow->getConduit();
       $parser = new ArcanistDiffParser();
 
-      $repository_api->parseRelativeLocalCommit(
+      $this->repositoryApi->parseRelativeLocalCommit(
         $workflow->getArgument('paths')
       );
-      $log = $repository_api->getGitCommitLog();
+      $log = $this->repositoryApi->getGitCommitLog();
       $changes = $parser->parseDiff($log);
 
       // Number of valid (Differential formatted) commits in given range.
@@ -59,7 +76,8 @@ class ArcJIRAConfiguration extends ArcanistConfiguration {
         return;
       }
 
-      // No valid revision or only last revision is valid.
+      // If we got here, there is no valid revision or only last revision
+      // is valid.
 
       $change = reset($changes);
       if ($change->getType() != ArcanistDiffChangeType::TYPE_MESSAGE) {
@@ -71,7 +89,7 @@ class ArcJIRAConfiguration extends ArcanistConfiguration {
 
       $revision_id = $message->getRevisionID();
 
-      $message->pullDataFromConduit($conduit);
+      $message->pullDataFromConduit($this->conduit);
 
       $match = null;
       // Get JIRA ID from the commit message if provided.
@@ -85,70 +103,13 @@ class ArcJIRAConfiguration extends ArcanistConfiguration {
         return;
       }
 
-      $jira_id = null;
-      // Get JIRA ID from command line.
-      if ($workflow->getArgument('jira')) {
-        $jira_id = $workflow->getArgument('jira');
-      }
-
-      // There is no JIRA ID in either commit message, or command line.
-      // Abort execution of custom code.
-      if (!$jira_id) {
-        return;
-      }
-
-      $match = null;
-      preg_match(
-        '/^[[:alnum:]]+-[[:digit:]]+$/',
-        $jira_id,
-        $match
-      );
-      // User didn't provide full ID with project name and issue number.
-      if (!$match) {
-        $jira_project = $workflow->getWorkingCopy()->getConfig('jira_project');
-        if (!$jira_project) {
-          throw new ArcanistUsageException(
-            'You haven\'t provided full JIRA ID with project name and issue '.
-            'number, and `jira_project` is not set in `.arcconfig`.  Use '.
-            'full JIRA ID like `--jira HIVE-2486` or set `jira_project` and '.
-            'use `--jira 2486`.'
-          );
-        }
-
-        $match = null;
-        preg_match(
-          '/^[[:digit:]]+$/',
-          $jira_id,
-          $match
-        );
-        if (!$match) {
-          throw new ArcanistUsageException(
-            'Provided JIRA ID is in wrong format, either specify full ID '.
-            'with project name and issue number like `--jira HIVE-2486` or '.
-            'provide only the issue number like `--jira 2486`.'
-          );
-        }
-
-        $jira_id = $jira_project . '-' . $jira_id;
-      }
-      $message->setFieldValue('jira', $jira_id);
-
-      $jira_api_url = $workflow->getWorkingCopy()->getConfig('jira_api_url');
-
-      if (!$jira_api_url) {
-        throw new ArcanistUsageException(
-          'To use `--jira` switch, you have to set `jira_api_url` in your '.
-          '`.arcconfig` file'
-        );
-      }
-
       // CC and Reviewers are messed up in $message->getFields() - they use
       // PHIDs instead of normal user names.  They have to be converted back.
       if (array_key_exists('ccPHIDs', $message->getFields())) {
         $ccPHIDs = $message->getFieldValue('ccPHIDs');
         $cc = array();
         foreach ($ccPHIDs as $phid) {
-          $cc[] = $this->userPHIDToName($conduit, $phid);
+          $cc[] = $this->userPHIDToName($phid);
         }
         $message->setFieldValue('ccPHIDs', $cc);
       }
@@ -156,7 +117,7 @@ class ArcJIRAConfiguration extends ArcanistConfiguration {
         $reviewerPHIDs = $message->getFieldValue('reviewerPHIDs');
         $reviewer = array();
         foreach ($reviewerPHIDs as $phid) {
-          $reviewer[] = $this->userPHIDToName($conduit, $phid);
+          $reviewer[] = $this->userPHIDToName($phid);
         }
         $message->setFieldValue('reviewerPHIDs', $reviewer);
       }
@@ -174,55 +135,8 @@ class ArcJIRAConfiguration extends ArcanistConfiguration {
         $message->setFieldValue('testPlan', 'EMPTY');
       }
 
-      // Pull title and description from JIRA
-      $curl = curl_init(
-        $jira_api_url
-        . 'jira.issueviews:issue-xml/'
-        . $message->getFieldValue('jira')
-        . '/'
-        . $message->getFieldValue('jira')
-        . '.xml?field=title&field=description'
-      );
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-      $issue = curl_exec($curl);
-      try {
-        // Will fail both when we fail to connect and if we get non XML
-        // response.
-        $issue = new SimpleXMLElement($issue, LIBXML_NOERROR);
-      } catch (Exception $e) {
-        throw new Exception(
-          'Failed to get issue information from JIRA.  Check if you can '.
-          'connect to used JIRA instance and check if given JIRA ID doesn\'t'.
-          'contain errors.'
-        );
-      }
-      $title = $issue->channel->item->title;
-      $description = $issue->channel->item->description;
-
-      if (!$title) {
-        throw new Exception('Failed to get issue title from JIRA.');
-      }
-      $title = preg_replace(
-        '/\[[[:alnum:]]+-[[:digit:]]+\](.*)/',
-        '$1',
-        $title
-      );
-      $title = trim(html_entity_decode($title, ENT_QUOTES));
-
-      // Different issue types have or don't have descriptions.  Bugs, New
-      // Features and Improvements do, Tasks don't, so don't panic if it fails.
-      if (!$description) {
-        $description = '';
-      }
-      // Remove HTML markup.
-      $description = str_replace("\n", ' ', $description);
-      $description = str_replace('<br/>', "\n", $description);
-      $description = str_replace('<p>', "\n\n", $description);
-      $description = str_replace('</p>', ' ', $description);
-      $description = preg_replace('/<a[^>]*>([^<]*)<\/a>/', '$1', $description);
-      $description = preg_replace("/\n */", "\n", $description);
-      $description = trim(html_entity_decode($description, ENT_QUOTES));
-
+      $title = $this->jiraInfo['title'];
+      $description = $this->jiraInfo['description'];
       $commit_title = $message->getFieldValue('title');
       $commit_summary = $message->getFieldValue('summary');
 
@@ -234,7 +148,7 @@ class ArcJIRAConfiguration extends ArcanistConfiguration {
       );
 
       $fields = $message->getFields();
-      $msg = $fields['jira'] . ' [jira] ' . $fields['title'];
+      $msg = $this->jiraId . ' [jira] ' . $fields['title'];
 
       if (array_key_exists('summary', $fields)) {
         $msg .= "\n\nSummary:\n" . $fields['summary'];
@@ -256,12 +170,279 @@ class ArcJIRAConfiguration extends ArcanistConfiguration {
         $msg .= "\n\nDifferential Revision: " . $fields['revisionID'];
       }
 
-      $repository_api->amendGitHeadCommit($msg);
+      $this->repositoryApi->amendGitHeadCommit($msg);
     }
   }
 
-  private function userPHIDToName($conduit, $phid) {
-    $user = $conduit->callMethodSynchronous(
+
+  public function didRunWorkflow(
+      $command,
+      ArcanistBaseWorkflow $workflow,
+      $err
+    ) {
+    if ($workflow instanceof ArcanistDiffWorkflow) {
+      // Magic in the post hook happens only for SVN.
+      if (!($this->repositoryApi instanceof ArcanistSubversionAPI)) {
+        return;
+      }
+
+      $diff_id = $workflow->getDiffID();
+
+      $commitable_revisions = $this->conduit->callMethodSynchronous(
+        'differential.find',
+        array(
+          'query' => 'commitable',
+          'guids' => array($workflow->getUserPHID())
+        )
+      );
+      $open_revisions = $this->conduit->callMethodSynchronous(
+        'differential.find',
+        array(
+          'query' => 'open',
+          'guids' => array($workflow->getUserPHID())
+        )
+      );
+      $revisions_data = array_merge($open_revisions, $commitable_revisions);
+      $revisions = array();
+      foreach ($revisions_data as $revision) {
+        $ref = ArcanistDifferentialRevisionRef::newFromDictionary($revision);
+        $revisions[$ref->getId()] = $ref;
+      }
+
+      $revision = null;
+
+      if (count($revisions) == 0) {
+        // $revision stays null.
+        if (!$this->jiraId) {
+          // Just create the diff.
+          return;
+        }
+      } else {
+        // Choose revision from available ones.
+        $choices = array();
+        echo "\n\n";
+        $ii = 1;
+        foreach ($revisions as $ref) {
+          $choices[$ii] = $ref;
+          echo ' ['.$ii++.'] D'.$ref->getID().' '.$ref->getName()."\n";
+        }
+        if ($this->jiraId !== null) {
+          $choices[$ii] = null;
+          echo ' ['.$ii++."] Create a new revision\n";
+        }
+        echo ' ['.$ii."] Don't attach this diff to any revision\n";
+
+        $valid_choice = false;
+        while (!$valid_choice) {
+          $id = phutil_console_prompt('Which revision do you want to update?');
+          $id = trim(strtoupper($id));
+          if (strpos($id, 'D') === 0) {
+            $id = trim($id, 'D');
+            if (isset($revisions[$id])) {
+              $valid_choice = true;
+              $revision = $revisions[$id];
+            }
+          } else {
+            $id = intval($id);
+            if ($id > 0 && $id < $ii) {
+              $valid_choice = true;
+              $revision = $choices[$id];
+            }
+            if ($id == $ii) {
+              // Just create the diff.
+              return;
+            }
+          }
+        }
+      }
+
+      $this->getComment();
+
+      if ($revision === null) {
+        // Create a new revision.
+        $title = $this->jiraInfo['title'];
+        $description = $this->jiraInfo['description'];
+
+        $summary = $this->comment . "\n\n" . $description;
+
+        $jira_phid = $this->conduit->callMethodSynchronous(
+          'user.find',
+          array(
+            'aliases' => array('JIRA')
+          )
+        );
+        $jira_phid = $jira_phid['JIRA'];
+
+        $revision = array(
+          'diffid' => $diff_id,
+          'fields' => array(
+            'title' => $this->jiraId . ' [jira] ' . $title,
+            'summary' => $summary,
+            'reviewerPHIDs' => array($jira_phid),
+            'testPlan' => 'EMPTY'
+          )
+        );
+
+        $result = $this->conduit->callMethodSynchronous(
+          'differential.createrevision',
+          $revision
+        );
+
+        echo "\n\nCreated new revision D".$result['revisionid']."\n";
+        echo $result['uri']."\n";
+      } else {
+        // Update existing revision.
+        $result = $this->conduit->callMethodSynchronous(
+          'differential.updaterevision',
+          array(
+            'id' => $revision->getId(),
+            'diffid' => $diff_id,
+            'fields' => array(),
+            'message' => $this->comment
+          )
+        );
+
+        echo "\n\nUpdated revision D".$result['revisionid']."\n";
+        echo $result['uri']."\n";
+      }
+    }
+  }
+
+  private function getJiraId() {
+      // Get JIRA ID from command line.
+      $jiraId = $this->workflow->getArgument('jira');
+
+      // No JIRA ID provided.
+      if (!$jiraId) {
+        return;
+      }
+
+      $match = null;
+      preg_match(
+        '/^[[:alnum:]]+-[[:digit:]]+$/',
+        $jiraId,
+        $match
+      );
+
+      // User didn't provide full ID with project name and issue number.
+      if (!$match) {
+        $jira_project = $this->workflow->getWorkingCopy()
+          ->getConfig('jira_project');
+        if (!$jira_project) {
+          throw new ArcanistUsageException(
+            'You haven\'t provided full JIRA ID with project name and issue '.
+            'number, and `jira_project` is not set in `.arcconfig`.  Use '.
+            'full JIRA ID like `--jira HIVE-2486` or set `jira_project` and '.
+            'use `--jira 2486`.'
+          );
+        }
+
+        $match = null;
+        preg_match(
+          '/^[[:digit:]]+$/',
+          $jiraId,
+          $match
+        );
+        if (!$match) {
+          throw new ArcanistUsageException(
+            'Provided JIRA ID is in wrong format, either specify full ID '.
+            'with project name and issue number like `--jira HIVE-2486` or '.
+            'provide only the issue number like `--jira 2486`.'
+          );
+        }
+
+        $jiraId = $jira_project . '-' . $jiraId;
+      }
+
+      $this->jiraId = $jiraId;
+  }
+
+  private function getJiraApiUrl() {
+    $jiraApiUrl = $this->workflow->getWorkingCopy()
+      ->getConfig('jira_api_url');
+
+    if (!$jiraApiUrl) {
+      throw new ArcanistUsageException(
+        'To use `--jira` switch, you have to set `jira_api_url` in your '.
+        '`.arcconfig` file'
+      );
+    }
+
+    $this->jiraApiUrl = $jiraApiUrl;
+  }
+
+  private function getJiraInfo() {
+    // Pull title and description from JIRA
+    $curl = curl_init(
+      $this->jiraApiUrl
+      . 'jira.issueviews:issue-xml/'
+      . $this->jiraId
+      . '/'
+      . $this->jiraId
+      . '.xml?field=title&field=description'
+    );
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $issue = curl_exec($curl);
+    try {
+      // Will fail both when we fail to connect and if we get non XML
+      // response.
+      $issue = new SimpleXMLElement($issue, LIBXML_NOERROR);
+    } catch (Exception $e) {
+      throw new Exception(
+        'Failed to get issue information from JIRA.  Check if you can '.
+        'connect to used JIRA instance and check if given JIRA ID doesn\'t '.
+        'contain errors.'
+      );
+    }
+    $title = $issue->channel->item->title;
+    $description = $issue->channel->item->description;
+
+    if (!$title) {
+      throw new Exception('Failed to get issue title from JIRA.');
+    }
+    $title = preg_replace(
+      '/\[[[:alnum:]]+-[[:digit:]]+\](.*)/',
+      '$1',
+      $title
+    );
+    $title = trim(html_entity_decode($title, ENT_QUOTES));
+
+    // Different issue types have or don't have descriptions.  Bugs, New
+    // Features and Improvements do, Tasks don't, so don't panic if it fails.
+    if (!$description) {
+      $description = '';
+    }
+    // Remove HTML markup.
+    $description = str_replace("\n", ' ', $description);
+    $description = str_replace('<br/>', "\n", $description);
+    $description = str_replace('<p>', "\n\n", $description);
+    $description = str_replace('</p>', ' ', $description);
+    $description = preg_replace('/<a[^>]*>([^<]*)<\/a>/', '$1', $description);
+    $description = preg_replace("/\n */", "\n", $description);
+    $description = trim(html_entity_decode($description, ENT_QUOTES));
+
+    $this->jiraInfo = array(
+      'title' => $title,
+      'description' => $description,
+    );
+  }
+
+  private function getComment() {
+    $template =
+      "\n\n".
+      '# Enter a brief description of the changes included in this update.'.
+      "\n";
+
+    $comment = id(new PhutilInteractiveEditor($template))
+      ->editInteractively();
+    $comment = preg_replace('/^\s*#.*$/m', '', $comment);
+    $comment = rtrim($comment);
+
+    $this->comment = $comment;
+  }
+
+  private function userPHIDToName($phid) {
+    $user = $this->conduit->callMethodSynchronous(
       'user.info',
       array(
         'phid' => $phid
--- arc_jira_lib/arcanist/__init__.php
+++ arc_jira_lib/arcanist/__init__.php
@@ -8,9 +8,13 @@
 
 phutil_require_module('arcanist', 'configuration');
 phutil_require_module('arcanist', 'differential/commitmessage');
+phutil_require_module('arcanist', 'differential/revision');
+phutil_require_module('arcanist', 'exception/usage');
 phutil_require_module('arcanist', 'parser/diff');
 phutil_require_module('arcanist', 'parser/diff/changetype');
 
+phutil_require_module('phutil', 'console');
+phutil_require_module('phutil', 'console/editor');
 phutil_require_module('phutil', 'utils');
 
 
